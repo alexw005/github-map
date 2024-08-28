af481bdb4329ffd5920d937044f81fe7dd2c26b2
alexw005 <fswoon2@gmail.com>
2024-08-28 21:55:29 +0800

    cleanup

--- src/lib/helper/loadCountryTask.ts
+++ src/lib/helper/loadCountryTask.ts
@@ -29,8 +29,6 @@ interface GithubCountry {
 const { features } = countries as { features: Country[] };
 
 class LoadCountryTask {
-  private covidUrl: string =
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv";
   private githubApiUrl: string = `https://api.github.com/search/users?q=location:`;
   private setState: ((features: Country[]) => void) | null = null;
   private fetchGithubData = async (searchString: string) => {
@@ -47,42 +45,13 @@ class LoadCountryTask {
       console.error("Error fetching data:", error);
     }
   };
-  public load(setState: (features: Country[]) => void): void {
-    this.setState = setState;
 
-    papa.parse<CovidCountry>(this.covidUrl, {
-      download: true,
-      header: true,
-      complete: (result: any) => this.processCovidData(result.data),
-    });
-  }
   public loadGithub(searchString: string, setState: (features: Country[]) => void): void {
     this.setState = setState;
     this.fetchGithubData(searchString);
   }
 
-  private processCovidData(covidCountries: CovidCountry[]): void {
-    for (let i = 0; i < features.length; i++) {
-      const country: Country = features[i];
-      const covidCountry = covidCountries.find(
-        (covidCountry) => country.properties.ISO_A3 === covidCountry.ISO3,
-      );
 
-      country.properties.confirmed = 0;
-      country.properties.confirmedText = "0";
-
-      if (covidCountry != null) {
-        let confirmed = Number(covidCountry.Confirmed);
-        country.properties.confirmed = confirmed;
-        country.properties.confirmedText = this.formatNumberWithCommas(confirmed);
-      }
-      this.setCountryColor(country);
-    }
-
-    if (this.setState) {
-      this.setState(features);
-    }
-  }
   private processGithubData(githubCountry: GithubCountry): void {
     if (githubCountry.country.trim() !== "") {
       const country: Country = features.find(
