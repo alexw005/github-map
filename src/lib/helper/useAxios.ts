67e18f56f1cb9a1fe19032f7bea0e2e67276967b
alexw005 <fswoon2@gmail.com>
2024-09-07 01:58:28 +0800

    use axios make sure new value

--- src/lib/helper/useAxios.ts
+++ src/lib/helper/useAxios.ts
@@ -1,29 +1,35 @@
-//axios hook
-
 import { useState, useEffect } from 'react';
-
 import axios from 'axios';
 
 export const useAxios = (url: string) => {
     const [data, setData] = useState<any>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<any>(null);
+
     const fetchData = async () => {
         try {
             const response = await axios.get(url);
-            setData(response.data);
+            const newData = response.data;
+
+            // Only update state if newData is different
+            if (JSON.stringify(newData) !== JSON.stringify(data)) {
+                setData(newData);
+            }
         } catch (error) {
             setError(error);
         } finally {
             setLoading(false);
         }
     };
+
     useEffect(() => {
-
-
         fetchData();
     }, [url]);
-    const refetch = () => { fetchData() };
+
+    const refetch = () => {
+        setLoading(true); // Set loading true when refetching
+        fetchData();
+    };
 
     return { data, loading, error, refetch };
 };
