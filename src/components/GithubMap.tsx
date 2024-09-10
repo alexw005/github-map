0f7b59eed99b79553c4b34218375221a48d63e50
alexw005 <fswoon2@gmail.com>
2024-09-11 01:36:01 +0800

    get optionList

--- src/components/GithubMap.tsx
+++ src/components/GithubMap.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { useCallback, useEffect, useState } from "react";
 
-import LoadCountryTask, { Feature } from "@/lib/helper/loadCountryTask";
 import GeoMap from "./GeoMap";
 import SearchTextInput from "./SearchTextInput";
 import { useSearchParams } from "next/navigation";
 import Loading from "./Loading";
+import useAxios from "@/lib/helper/useAxios";
+import { API_BASE_URL } from "@/lib/helper/constant";
 
 const GithubMap = () => {
   const searchParams = useSearchParams()
+  const { data: optionList, loading: loadingOption, refetch: fetchOptions } = useAxios(`${API_BASE_URL}/list/countries`);
 
   const [searchText, setSearchText] = useState<string>('');
   const search = searchParams.get('search')
@@ -19,12 +21,15 @@ const GithubMap = () => {
   const handleSearch = (v: string) => {
     setSearchText(v);
   }
+  if (loadingOption) {
+    return <Loading />;
+  }
   return (
     <div>
-      {searchText.trim() === '' ? <SearchTextInput onSearch={handleSearch} description="Search country for the total number of github users" />
+      {searchText.trim() === '' ? <SearchTextInput onSearch={handleSearch} description="Search country for the total number of github users" options={optionList} />
         : (
           <div>
-            <GeoMap searchText={searchText} />
+            <GeoMap searchText={searchText} optionList={optionList} />
           </div>
         )}
     </div>
