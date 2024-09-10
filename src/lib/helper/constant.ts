9b3f6acf97c11c92b78116866dbdb66a7b142500
alexw005 <fswoon2@gmail.com>
2024-09-11 01:36:08 +0800

    change constant

--- src/lib/helper/constant.ts
+++ src/lib/helper/constant.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
     process.env.NODE_ENV === "production"
         ? process.env.NEXT_PUBLIC_API_URL
-        : "http://localhost:8000";
+        : "http://localhost:8000/api/v1";
