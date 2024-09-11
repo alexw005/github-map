7bc73c5847bdb50b7960167d1b07361736eb3f4a
alexw005 <fswoon2@gmail.com>
2024-09-12 00:46:02 +0800

    fix geo map

--- src/components/GithubMap.tsx
+++ src/components/GithubMap.tsx
@@ -3,24 +3,18 @@ import { useCallback, useEffect, useState } from "react";
 
 import GeoMap from "./GeoMap";
 import SearchTextInput from "./SearchTextInput";
-import { useSearchParams } from "next/navigation";
 import Loading from "./Loading";
 import useAxios from "@/lib/helper/useAxios";
 import { API_BASE_URL } from "@/lib/helper/constant";
 
 const GithubMap = () => {
-  const searchParams = useSearchParams()
   const { data: optionList, loading: loadingOption, refetch: fetchOptions } = useAxios(`${API_BASE_URL}/list/countries`);
-
   const [searchText, setSearchText] = useState<string>('');
-  const search = searchParams.get('search')
-  if (search && search !== searchText) {
-    setSearchText(search)
-  }
 
   const handleSearch = (v: string) => {
     setSearchText(v);
   }
+
   if (loadingOption) {
     return <Loading />;
   }
